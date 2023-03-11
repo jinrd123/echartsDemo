830bb4737f7f5c69e229826810302272650b2d23
jinrongda <jinrongda@baidu.com>
2023-03-11 19:28:47 +0800

    右侧折线图

--- js/index.js
+++ js/index.js
@@ -245,7 +245,7 @@
   ];
 
   // 1. 实例化对象
-  const  myChart = echarts.init(document.querySelector(".line .chart"));
+  const myChart = echarts.init(document.querySelector(".line .chart"));
   // 2.指定配置
   const option = {
     // 对应series中两个对象，即两条线的颜色
@@ -354,10 +354,220 @@
   const aTags = document.querySelectorAll(".line a");
   aTags.forEach((aTag, index) => {
     aTag.addEventListener("click", () => {
-        option.series.forEach((seriesItem, seriesIndex) => {
-            seriesItem.data = yearData[index].data[seriesIndex];
-        })
-        myChart.setOption(option); // 调用setOption方法重新渲染
-    })
+      option.series.forEach((seriesItem, seriesIndex) => {
+        seriesItem.data = yearData[index].data[seriesIndex];
+      });
+      myChart.setOption(option); // 调用setOption方法重新渲染
+    });
+  });
+})();
+
+// 右侧折线图模块
+/**
+ *  新的学习点：
+ *  1. 关于顶层配置项color、series.itemStyle.color、series.lineStyle.color
+ *      如果只存在顶层color，series中其它任何可以设置color的配置都没有指定具体的color，那么series.itemStyle和series.lineStyle等可指定具体color的地方都使用顶层color
+ *      如果存在顶层color，series.itemStyle设置了color，但是series.lineStyle没有设置color，那么series.lineStyle使用series.itemStyle的color
+ *  2. 新学习配置项
+ *      1）: series.areaStyle：线条与x轴之间区域的相关配置
+ *      2）: series.symbol...：拐点相关设置
+ */
+(function () {
+  const myChart = echarts.init(document.querySelector(".line2 .chart"));
+  const option = {
+    tooltip: {
+      trigger: "axis",
+    },
+    color: ["red", 'black'],
+    legend: {
+      top: "0%",
+      data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"],
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12",
+      },
+    },
+
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: [
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "26",
+          "28",
+          "29",
+          "30",
+        ],
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)",
+          },
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        name: "邮件营销",
+        type: "line",
+        smooth: true,
+        // 单独修改当前线条的样式
+        lineStyle: {
+          color: "#0184d5",
+          width: "2",
+        },
+        // 填充颜色设置
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(1, 132, 213, 0.4)", // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: "rgba(1, 132, 213, 0.1)", // 渐变线的结束颜色
+              },
+            ],
+            false
+          ),
+          shadowColor: "rgba(0, 0, 0, 0.1)",
+        },
+
+        /**
+         * 拐点相关配置
+         */
+        // 设置拐点
+        symbol: "circle",
+        // 拐点大小
+        symbolSize: 8,
+        // 默认不显示拐点，鼠标经过时显示
+        showSymbol: false,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: "#0184d5",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12,
+        },
+        data: [
+          30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 30,
+          60, 20, 40, 30, 40, 30, 40, 30, 40, 20, 60, 50, 40,
+        ],
+      },
+      {
+        name: "联盟广告",
+        type: "line",
+        smooth: true,
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 2,
+          },
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)",
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)",
+                },
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)",
+          },
+        },
+        symbol: "circle",
+        symbolSize: 5,
+        itemStyle: {
+          color: "#00d887",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12,
+        },
+        // 开始不显示拐点， 鼠标经过显示
+        showSymbol: false,
+        data: [
+          130, 10, 20, 40, 30, 40, 80, 60, 20, 40, 90, 40, 20, 140, 30, 40, 130,
+          20, 20, 40, 80, 70, 30, 40, 30, 120, 20, 99, 50, 20,
+        ],
+      },
+    ],
+  };
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
   });
 })();
